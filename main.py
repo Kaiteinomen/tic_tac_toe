e4f6d7f7082d3fd9b28fd83e6ff4229a5fd43927
Kaiteinomen <foxer202@hotmail.com>
2017-01-09 10:16:21 +0800

    username&token done

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import falcon
+import json
+map_start = [0,0,0,0,0,0,0,0,0]
+token = {u'abc' : 1, u'def' : 2}
 class Resource(object):
     def on_get(self, req, resp):
-        resp.body = '{"message":"hello world"}'
+        resp.body = str(map_start)
         resp.status = falcon.HTTP_200
+    def on_post(self, req, resp):
+        
+        data = json.load(req.stream)     
+        print repr(data)
+        print repr(data['username'])
+        map_start[int(data['number']) - 1] = token[data['username']]
+        print repr(map_start)
 r = Resource()
 api = falcon.API()
 api.add_route('/', r)
